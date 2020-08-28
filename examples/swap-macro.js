4a104bef556373d439a1684db8966ee8c01ce06f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-08-28 06:28:35 -0500

    run resugare mods
    - declarations.block-scope: Transform `var` into `let` and `const` as appropriate.
    - functions.arrow         : Transform regular functions to arrow functions as appropriate.
    - objects.concise         : Use concise object property method syntax.
    - objects.destructuring   : Transform some declarations and assignments to the more compact destructuring form.
    - objects.shorthand       : Use shorthand notation for object properties.
    - strings.template        : Transforms manual string concatenation into template strings.
    
    ```ps1
    ❯ $all= (ls -r )
    >> foreach($obj in $all) {
    >> if ($obj.extension -eq '.js') {
    >> $file = $obj.fullname
    >> esnext -o $file $file -b modules.commonjs
    >> }
    >> }
    ```

--- examples/swap-macro.js
+++ examples/swap-macro.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-var recast = require('recast');
-var util = require('../lib');
-var types = util.types;
-var n = types.namedTypes;
-var b = types.builders;
-var assert = require('assert');
+const recast = require('recast');
+const util = require('../lib');
+const { types } = util;
+const n = types.namedTypes;
+const b = types.builders;
+const assert = require('assert');
 
 /**
  * Treats calls to `swap` as a macro to swap the identifiers passed to swap.
@@ -23,28 +23,28 @@ var assert = require('assert');
  * @return {ast-types.Node} Returns `ast`.
  */
 function transform(ast) {
-  var replaced = [];
+  const replaced = [];
 
   types.traverse(ast, function(node) {
     if (n.ExpressionStatement.check(node)) {
       if (isSwapCall(this.get('expression'))) {
         // swap(left, right)
-        var expression = node.expression;
-        assert.equal(expression.arguments.length, 2, 'expected 2 arguments to `swap`, got ' + expression.arguments.length);
+        const { expression } = node;
+        assert.equal(expression.arguments.length, 2, `expected 2 arguments to \`swap\`, got ${expression.arguments.length}`);
 
-        var left = expression.arguments[0];
-        var right = expression.arguments[1];
+        const left = expression.arguments[0];
+        const right = expression.arguments[1];
 
         assert.ok(
           n.Identifier.check(left) || n.MemberExpression.check(left),
-          'expected first argument of `swap` to be an Identifier or MemberExpression, found ' + left.type
+          `expected first argument of \`swap\` to be an Identifier or MemberExpression, found ${left.type}`
         );
         assert.ok(
           n.Identifier.check(right) || n.MemberExpression.check(right),
-          'expected second argument of `swap` to be an Identifier or MemberExpression, found ' + right.type
+          `expected second argument of \`swap\` to be an Identifier or MemberExpression, found ${right.type}`
         );
 
-        var tmp = util.uniqueIdentifier(this.scope);
+        const tmp = util.uniqueIdentifier(this.scope);
 
         replaced.push(expression);
         this.replace(
@@ -80,12 +80,12 @@ function isSwapCall(path) {
 }
 
 function readStdin(callback) {
-  var stdin = '';
+  let stdin = '';
 
   process.stdin.setEncoding('utf8');
 
   process.stdin.on('readable', function() {
-    var chunk = process.stdin.read();
+    const chunk = process.stdin.read();
     if (chunk !== null) {
       stdin += chunk;
     }
